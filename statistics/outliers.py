9facd4c4e91aaa4599f8c5d12256f3c8744e4d52
abeker <acabeker@gmail.com>
2021-03-13 14:53:27 +0100

    outliers statistics

--- statistics/outliers.py
+++ statistics/outliers.py
@@ -19,3 +19,24 @@ def subplots(dataframe):
     ax = sns.boxplot(data=df, orient="h", palette="Set2")
     ax.plot()
     plt.show()
+
+def scatter_plot(dataframe, column1, column2):
+    fig, ax = plt.subplots(figsize=(16, 8))
+    ax.scatter(dataframe[column1], dataframe[column2])
+    ax.set_xlabel(column1)
+    ax.set_ylabel(column2)
+    plt.show()
+
+def pairplot(dataframe):
+    sns.pairplot(dataframe)
+    plt.show()
+
+def cap_outliers(column, zscore_threshold=2):
+    median_val = column.median()
+    mad_val = column.mad()
+    st_dev = column.mad() * 1.4826
+    z_score = (column - column.mean()) / st_dev
+    outliers = abs(z_score) > zscore_threshold
+    df_outliers = column.loc[outliers]
+    df_outliers = column[column.index.isin(df_outliers.index)]
+    print(df_outliers.to_string())
