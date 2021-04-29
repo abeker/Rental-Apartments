d5dd14a9790cb0a792235e20256a1c24951d346f
bokimilinkovic <boki1997@gmail.com>
2021-04-29 14:21:30 +0200

    Adaboost near end

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import statistics.outliers as outliers
 import statistics.corelation as corelation
 from algorithms import regression_impl
 from algorithms import xgboost_impl
+from algorithms import adaboost_impl
 import utility.enums as enum
 
 df_munich = munich_handler.get_munich_data()
 # outliers.get_outliers_statistics(df_munich)
-corelation.print_df_corelation(df_munich)
+# corelation.print_df_corelation(df_munich)
 # df_berlin = berlin.get_berlin_dataset()
-model = regression_impl.train(df_munich, enum.RegressionType.LINEAR)
-xgboost_impl.train(df_munich, True, True)
+# model = regression_impl.train(df_munich, enum.RegressionType.LINEAR)
+# xgboost_impl.train(df_munich, True, True)
+adaboost_impl.train(df_munich, True, True)
