219598245f8788780b9fc7539987751511895452
abeker <acabeker@gmail.com>
2021-05-04 12:57:24 +0200

    class creation

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import munich_handler
-import statistics.outliers as outliers
-import statistics.corelation as corelation
 from algorithms import regression_impl
-from algorithms import xgboost_impl
+from algorithms.neural_network import neural_networks_impl
 import utility.enums as enum
 
 df_munich = munich_handler.get_munich_data()
-corelation.print_df_corelation(df_munich)
+# corelation.print_df_corelation(df_munich)
+# outliers.get_outliers_statistics(df_munich)
+
 # df_berlin = berlin.get_berlin_dataset()
 model = regression_impl.train(df_munich, enum.RegressionType.LINEAR)
-outliers.get_outliers_statistics(df_munich)
-xgboost_impl.train(df_munich, True, True)
+# xgboost_impl.train(df_munich, True, True)
+print('-------------------------------')
+neural_networks_impl.train(df_munich)
