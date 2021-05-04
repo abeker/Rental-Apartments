fe3391abe92c37c12d22489f8a8bfbc28ba5d73d
bokimilinkovic <boki1997@gmail.com>
2021-05-04 22:47:58 +0200

    Adaboost train

--- algorithms/adaboost_impl.py
+++ algorithms/adaboost_impl.py
@@ -9,13 +9,30 @@ from sklearn import metrics
 from sklearn.svm import SVC
 from algorithms import regression_impl as reg
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 
 iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
+# print(X.shape)
+# print(Y.shape)
 
 train_ratio = 0.70
 test_ratio = 0.30
+
+def prepare_data(dataset):
+   # divides data into attributes and labels:
+   count_col = dataset.shape[1]
+   X = dataset.iloc[:, 1:count_col].values
+   y = dataset.iloc[:, 0].values
+   #divide the data into training and testing sets
+   X_train, y_train, X_test, y_test, x_val, y_val = reg.split_dataset(X, y)
+   # Feature Scaling
+   sc = StandardScaler()
+   X_train = sc.fit_transform(X_train)
+   X_test = sc.transform(X_test)
+   return X_train, X_test, y_train, y_test
 
 def extract_df(df):
     price_column = df.loc[:, 'price'].values
@@ -30,48 +47,46 @@ def split_dataset(x, y):
 
 
 # def train_sample(params, dtrain, num_boost_round):
-def train(dataframe, to_predict=True, print_stats=True):
+def train(dataframe):
+    train_columns = dataframe.drop('price', axis=1)
+    train_columns = train_columns.to_numpy()
+    price_column = dataframe.loc[:, 'price'].values
     x, y = extract_df(dataframe)
+    # print(x.shape)
+    # print(y.shape)
     x_train, y_train, x_test, y_test, x_val, y_val = reg.split_dataset(x,y)
-    x_train2, y_train2, x_test2, y_test2 = split_dataset(x,y)
+    # lab_enc = preprocessing.LabelEncoder()
+    # x = lab_enc.fit_transform(x)
+
+    print("PRICE")
+    print(price_column)
+    print(price_column.shape)
+    # x_train, y_train, x_test, y_test = split_dataset(train_columns, price_column)
+    print("TRAIN")
+    print(x_train)
+    print(x_train.shape)
+    print(y_train.shape)
     svc = SVC(probability=True, kernel='linear')
 
-    rng = np.random.RandomState(1)
-    print("XTRAIN")
-    print(x_train2.shape)
-    print(x_train2)
-    print("YTRAIN")
-    print(y_train2.shape)
-    print(y_train2)
-    print("x_test2")
-    print(x_test2.shape)
-    print(x_test2)
-    print("y_test2")
-    print(y_test2.shape)
-    print(y_test2)
+    # regr_1 = DecisionTreeRegressor(max_depth=4)
+    regr_2 = AdaBoostRegressor(n_estimators=300, random_state=0)
 
-    regr_1 = DecisionTreeRegressor(max_depth=4)
-    regr_2 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),n_estimators=300, random_state=rng)
+    abc = AdaBoostClassifier()
+    # model = abc.fit(x_train, y_train)
+    # regr_1.fit(x_train, y_train)
+    regr_2.fit(x_train, y_train)
 
-    abc = AdaBoostClassifier(n_estimators=100, random_state=0, learning_rate=1)
-    # model = abc.fit(x_train2, y_train2)
-    regr_1.fit(x_train2, y_train2)
-    regr_2.fit(x_train2, y_train2)
+    # y_1 = regr_1.predict(x_test)
+    # y_2 = regr_2.predict(x_test)
 
-    y_1 = regr_1.predict(x_test2)
-    y_2 = regr_2.predict(x_test2)
-
-    model1 = regr_1.fit(x_train2, y_train2)
-    model2 = regr_2.fit(x_train2, y_train2)
+    # model1 = regr_1.fit(x_train, y_train)
+    # model2 = regr_2.fit(x_train, y_train)
     # y_pred = model.predict(x_test);
     clf = RandomForestRegressor(n_estimators=10)
-    # y_pred2 = model.predict(x_test)
+    y_pred2 = regr_2.predict(x_test)
 
-    print("Y_PRED")
-    print(y_2)
-    print(y_2.shape)
-
-    # print("Accuracy:", model2.score(y_test2.reshape(-1,1), y_2.reshape(-1,1)))
+    print("Accuracy Square:", abs(metrics.r2_score(y_test, y_pred2)))
+    print("mean square log error:", metrics.mean_squared_log_error(y_test,y_pred2))
 
 
 
